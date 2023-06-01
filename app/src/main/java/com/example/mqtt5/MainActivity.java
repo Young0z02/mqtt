f022d7975cbc40cd07d9b59c172e07a5658733b5
Young0z02 <dmelf@naver.com>
2023-06-01 10:18:02 +0900

    Github finished

---
+++ app/src/main/java/com/example/mqtt5/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.mqtt5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String ServerIP = "tcp://223.195.194.41:1883";
+    private String TOPIC = "SF1";
+
+    private MqttClient mqttClient = null;
+    private TextView messageTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        messageTextView = findViewById(R.id.message_text_view);
+
+        Button button = findViewById(R.id.button_send);
+
+        try {
+            mqttClient = new MqttClient(ServerIP, MqttClient.generateClientId(), null);
+            mqttClient.connect();
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+                        mqttClient.publish(TOPIC, new MqttMessage("hello!".getBytes()));
+                    } catch (MqttException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            mqttClient.subscribe(TOPIC);
+            mqttClient.setCallback(new MqttCallback() {
+                @Override
+                public void connectionLost(Throwable throwable) {
+                    Log.d("MQTTService", "Connection Lost");
+                }
+
+                @Override
+                public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
+                    String message = "Message Arrived : " + mqttMessage.toString();
+                    Log.d("MQTTService", message);
+
+                    // 화면에 메시지 출력
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            messageTextView.setText(message);
+                        }
+                    });
+                }
+
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+                    Log.d("MQTTService", "Delivery Complete");
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
